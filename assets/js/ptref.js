0ed004dcfa7bbd80eff5c4a88f900fd5af09a17a
Pascal Rhod <pascal.rhod@canaltp.fr>
2015-12-27 02:05:12 +0100

    ptref - ajout de Etat du trafic et refonte gestion uri

--- assets/js/ptref.js
+++ assets/js/ptref.js
@@ -1,8 +1,9 @@
 ptref = function() {
     this.object_list=null,
+    this.disruption_list=null,
     this.object_count=-1,
     this.object_type="",
-    this.object_type_list = ["stop_points", "stop_areas", "pois", "poi_types", "networks", "lines", "routes", "vehicle_journeys", "physical_modes", "commercial_modes", "connections"],
+    this.object_type_list = ["stop_points", "stop_areas", "pois", "poi_types", "networks", "lines", "routes", "vehicle_journeys", "physical_modes", "commercial_modes", "connections", "traffic_reports" ],
     this.response = null,
     this.load = function(ws_name, coverage, uri, call_back){
         if (endsWith(uri, "/departures/")) {
@@ -29,6 +30,7 @@ ptref = function() {
             var names = Object.keys( response );
             if (object.object_type == "") {object.object_type=names[2];}
             object.object_list=eval("response\."+object.object_type);
+            object.disruption_list = response.disruptions;
             if (response.pagination) {
                 object.object_count = response.pagination.total_result;
             }
@@ -38,6 +40,17 @@ ptref = function() {
     }
 }
 
+function getSeverityIcon(disruption) {
+    if (disruption != "") {
+        title = "Severity: "+disruption.severity.effect + "\n" + "Message: " + disruption.messages[0].text; 
+        if (disruption.severity.effect == "NO_SERVICE"){
+            return '<img src="./assets/img/notification_error.png" title="'+title+'" height="20" width="20">';
+        } else {
+            return '<img src="./assets/img/warning.jpeg" title="'+title+'" height="20" width="20">';
+        }
+    }
+    return "";
+}
 
 function onMapClick(e) {
 	lamb=WGS_ED50(e.latlng.lng, e.latlng.lat);
@@ -62,33 +75,165 @@ function changeFormDataSubmit(){
 
 function changeURI(base_uri, object_id, uri_end) {
 	new_uri=base_uri+object_id+uri_end;
-	console.log(new_uri);
+    new_uri = new_uri.replace('%2F', '/');
+    new_uri = new_uri.replace('//', '/');
 	document.getElementById("uri").value=new_uri;
 	document.forms[0].submit();
 }
 
+function getNewURI(changed_uri, keep_current, current_id) {
+    url = location.href;
+    base_uri = url.substring(0, url.indexOf("?"));
+    params = url.substring(url.indexOf("?")+1, 1000);
+    params = params.split("&");
+    new_uri = "";
+    for (i in params) {
+        p = params[i];
+        if (p.split('=')[0] != 'uri'){
+            new_uri += p.split('=')[0] + "=" + p.split('=')[1]+"&";
+        } else {
+            if (keep_current) {
+                if (p.endsWith(current_id+'/')) { 
+                    new_uri += "uri" + "=" + p.split('=')[1] + changed_uri+"&";
+                } else {
+                    new_uri += "uri" + "=" + p.split('=')[1] + current_id + '/' + changed_uri+"&";
+                }
+            } else {
+                new_uri += "uri" + "=" + changed_uri+"&";
+            }
+        }
+    }
+    new_uri = new_uri.replace(/%2F/g, '/');
+    new_uri = new_uri.replace(/\/\//g, '/');
+    new_uri = base_uri + '?' + new_uri;
+    return new_uri;
+}
+
 function showNetworksHtml(){
+    disruptions = [];
+    for (var di in ptref.disruption_list){
+        disruption = ptref.disruption_list[di];
+        disruptions[disruption.id] = disruption; 
+    }
+
 	str="";
 	str+='<table><tr>';
 	str+='<th>Id (Nb : ' + ptref.object_list.length + ' / ' + ptref.object_count + ')</th>';
 	str+='<th>Name</th>';
 	str+='<th>Explorer</th>';
+	str+='<th></th>';
 	str+='</tr>';
 	for (var i in ptref.object_list){
 		n=ptref.object_list[i];
-
-		base_url=location.href;
-		if (!base_url.endsWith(n.id+'/')) { base_url+=n.id+'/'; }
-
 		s_str="<tr>";
-		s_str+='<td>'+n.id + "</td>";
+		s_str+='<td><a href="'+getNewURI('/networks/'+n.id+'/', false, n.id)+'">'+n.id+'</a></td>';
 		s_str+='<td>'+n.name + "</td>";
-		s_str+='<td><a href="'+base_url+'/physical_modes/'+'">Modes Ph</a></td>';
-		s_str+='<td><a href="'+base_url+'/commercial_modes/'+'">Modes Co</a></td>';
-		s_str+='<td><a href="'+base_url+'/lines/'+'">Lignes</a></td>';
-		s_str+='<td><a href="'+base_url+'/stop_areas/'+'">Zones d\'arrêts</a></td>';
+		s_str+='<td><a href="'+getNewURI('/physical_modes/', true, n.id)+'">Modes Ph</a></td>';
+		s_str+='<td><a href="'+getNewURI('/commercial_modes/', true, n.id)+'">Modes Co</a></td>';
+		s_str+='<td><a href="'+getNewURI('/lines/', true, n.id)+'">Lines</a></td>';
+		s_str+='<td><a href="'+getNewURI('/stop_areas/', true, n.id)+'">Zones d\'arrêts</a></td>';
+        worst_disruption = "";
+        for (var k in n.links){
+            d= n.links[k];
+            //on cherche la severité la plus forte : NO_SERVICE
+            if (d.type == 'disruption') {
+                if ((worst_disruption == "") || (worst_disruption.severity.effect != 'NO_SERVICE')) {
+                    worst_disruption = disruptions[d.id];
+                }
+            }
+        }
+		s_str+='<td>'+getSeverityIcon(worst_disruption)+'</td>';
 		s_str+="</tr>\n";
 		str+=s_str;
+	}
+	str+='</table>'
+	document.getElementById('ptref_content').innerHTML=str;
+}
+
+function showTrafficReportsHtml(){
+	str="";
+    disruptions = [];
+    for (var di in ptref.disruption_list){
+        disruption = ptref.disruption_list[di];
+        disruptions[disruption.id] = disruption; 
+    }
+
+	str+='<table><tr>';
+	str+='<th>Network</th>';
+	str+='<th>Line</th>';
+	str+='<th>Status</th>';
+	str+='<th>From</th>';
+	str+='<th>To</th>';
+	str+='<th>Updated</th>';
+	str+='<th>  </th>';
+	str+='</tr>';
+    for (var i in ptref.object_list){
+        //chaque élément contient toutes les perturbations d'un réseau
+		n=ptref.object_list[i];
+        network_id = n.network.id;
+        network_name = n.network.name;
+		
+        for (var j in n.lines) {
+            l=n.lines[j];
+            for (var k in l.links){
+                d=l.links[k];
+                s_str="<tr>";
+                s_str+='<td>'+'<a href="'+getNewURI('/networks/'+ network_id + '/', false)+'" ">'+ network_id + "</a>" + "</td>";
+                s_str+='<td>'+'<a href="'+getNewURI('/lines/'+ l.id + '/', false)+'" ">'+"<span class='icon-ligne' style='background-color: #"+l.color+";'>"+l.code + "</span>" +"</a></td>";
+                s_str+='<td>'+disruptions[d.id].status+'</td>';
+                s_str+='<td>'+disruptions[d.id].application_periods[0].begin+'</td>';
+                s_str+='<td>'+disruptions[d.id].application_periods[0].end+'</td>';
+                s_str+='<td>'+disruptions[d.id].updated_at+'</td>';
+                if (disruptions[d.id].severity.effect = "NO_SERVICE"){
+                    s_str+='<td><img src="./assets/img/notification_error.png" title="Severity: '+disruptions[d.id].severity.effect+'" height="20" width="20"></td>';
+                } else {
+                    s_str+='<td><img src="./assets/img/warning.jpeg" title="Severity: '+disruptions[d.id].severity.effect+'" height="20" width="20"></td>';
+                }
+                
+                s_str+="</tr>\n";
+                str+=s_str;
+            }
+        }
+	}
+	str+='</table>'
+
+	str+='<table><tr>';
+	str+='<th>Network</th>';
+	str+='<th>StopArea</th>';
+	str+='<th>Status</th>';
+	str+='<th>From</th>';
+	str+='<th>To</th>';
+	str+='<th>Updated</th>';
+	str+='<th>  </th>';
+	str+='</tr>';
+    for (var i in ptref.object_list){
+        //chaque élément contient toutes les perturbations d'un réseau
+		n=ptref.object_list[i];
+        network_id = n.network.id;
+        network_name = n.network.name;
+		
+        for (var j in n.stop_areas) {
+            sa=n.stop_areas[j];
+            for (var k in l.links){
+                d=l.links[k];
+                s_str="<tr>";
+                s_str+='<td>'+'<a href="'+getNewURI('/networks/'+network_id+'/', false)+'" >'+network_id + "</a>" + "</td>";
+//                s_str+='<td>'+network_id + "</td>";
+                s_str+='<td>'+'<a href="'+getNewURI('/stop_areas/'+sa.id+'/', false)+'">'+sa.name + "</a></td>";
+                s_str+='<td>'+disruptions[d.id].status+'</td>';
+                s_str+='<td>'+disruptions[d.id].application_periods[0].begin+'</td>';
+                s_str+='<td>'+disruptions[d.id].application_periods[0].end+'</td>';
+                s_str+='<td>'+disruptions[d.id].updated_at+'</td>';
+                if (disruptions[d.id].severity.effect = "NO_SERVICE"){
+                    s_str+='<td><img src="./assets/img/notification_error.png" title="Severity: '+disruptions[d.id].severity.effect+'" height="20" width="20"></td>';
+                } else {
+                    s_str+='<td><img src="./assets/img/warning.jpeg" title="Severity: '+disruptions[d.id].severity.effect+'" height="20" width="20"></td>';
+                }
+                
+                s_str+="</tr>\n";
+                str+=s_str;
+            }
+        }
 	}
 	str+='</table>'
 	document.getElementById('ptref_content').innerHTML=str;
@@ -103,13 +248,10 @@ function showModesHtml(){
 	str+='</tr>';
 	for (var i in ptref.object_list){
 		n=ptref.object_list[i];
-		base_url=location.href;
-		if (!base_url.endsWith(encodeURIComponent(n.id+'/'))) { base_url+=encodeURIComponent(n.id+'/'); }
-
 		s_str="<tr>";
 		s_str+='<td>'+n.id + "</td>";
 		s_str+='<td>'+n.name + "</td>";
-		s_str+='<td><a href="'+base_url+'lines/'+'">Lignes</a></td>';
+		s_str+='<td><a href="'+getNewURI('/lines/', true, n.id)+'">Lignes</a></td>';
 		s_str+="</tr>\n";
 		str+=s_str;
 		console.log(s_str);
@@ -119,21 +261,24 @@ function showModesHtml(){
 }
 
 function showStopAreasHtml(){
-	newBounds=[];
+    disruptions = [];
+    for (var di in ptref.disruption_list){
+        disruption = ptref.disruption_list[di];
+        disruptions[disruption.id] = disruption; 
+    }
+    newBounds=[];
 	str="";
 	str+='<table><tr>';
 	str+='<th>Id (Nb : ' + ptref.object_list.length + ' / ' + ptref.object_count + ')</th>';
 	str+='<th>Label</th>';
 	//str+='<th>City</th>';
 	str+='<th>Explorer</th>';
+	str+='<th></th>';
 	str+='</tr>';
 	for (var i in ptref.object_list){
 		n=ptref.object_list[i];
-		base_url=location.href;
-		if (!base_url.endsWith(n.id+'/')) { base_url+=n.id+'/'; }
 		s_str="<tr>";
-		console.log(n.id);
-		s_str+='<td><a href="'+base_url+'">'+n.id+'</a></td>';
+		s_str+='<td><a href="'+getNewURI('', true, n.id)+'">'+n.id+'</a></td>';
 		s_str+='<td>'+n.label + "</td>";
 		/*
 		s_str+='<td>';
@@ -143,13 +288,23 @@ function showStopAreasHtml(){
 		}
 		*/
 		s_str+='</td>';
-		s_str+='<td><a href="'+base_url+'/lines/'+'">Lignes</a></td>';
-		s_str+='<td><a href="'+base_url+'stop_points/'+'">StopPoints</a></td>';
-		s_str+='<td><a href="'+base_url+'connections/'+'">Corresp.</a></td>';
-		s_str+='<td><a href="'+base_url+'departures/'+'">Depart.</a></td>';
+		s_str+='<td><a href="'+getNewURI('/lines/', true, n.id)+'">Lignes</a></td>';
+		s_str+='<td><a href="'+getNewURI('/stop_points/', true, n.id)+'">StopPoints</a></td>';
+		s_str+='<td><a href="'+getNewURI('/connections/', true, n.id)+'">Corresp.</a></td>';
+		s_str+='<td><a href="'+getNewURI('/departures/', true, n.id)+'">Depart.</a></td>';
 		s_str+='<td><a href="stop_schedules.html?ws_name='+ws_name+'&coverage='+coverage+'&stop_area_id='+n.id+'">Horaires</a></td>';
-		s_str+='<td><a href="'+base_url+'places_nearby/'+'">Nearby</a></td>';
-		s_str+="</tr>\n";
+		s_str+='<td><a href="'+getNewURI('/places_nearby/', true, n.id)+'">Nearby</a></td>';
+        worst_disruption = "";
+        for (var k in n.links){
+            d= n.links[k];
+            //on cherche la severité la plus forte : NO_SERVICE
+            if (d.type == 'disruption') {
+                if ((worst_disruption == "") || (worst_disruption.severity.effect != 'NO_SERVICE')) {
+                    worst_disruption = disruptions[d.id];
+                }
+            }
+        }
+        s_str+="</tr>\n";
 		str+=s_str;
 		coord=n.coord;
 		n.marker = L.marker([coord.lat, coord.lon]).addTo(map);
@@ -188,19 +343,16 @@ function showStopPointsHtml(){
 	str+='</tr>';
 	for (var i in ptref.object_list){
 		n=ptref.object_list[i];
-		base_url=location.href;
-		if (!base_url.endsWith(encodeURIComponent(n.id)+'/')) { base_url+=encodeURIComponent(n.id)+'/'; }
 		s_str="<tr>";
-		s_str+='<td><a href="'+base_url+'">'+n.id+'</a></td>';
+		s_str+='<td><a href="'+getNewURI('', true, n.id)+'">'+n.id+'</a></td>';
 		s_str+='<td>'+n.label + "</td>";
-		s_str+='<td><a href="'+base_url+'/lines/'+'">Lignes</a></td>';
-		s_str+='<td><a href="#" onclick="changeURI(\'/stop_areas/'+n.stop_area.id+'/\',\'\', \'\')">StopArea</a></td>';
-		s_str+='<td><a href="'+base_url+'connections/'+'">Corresp.</a></td>';
-		s_str+='<td><a href="'+base_url+'departures/'+'">Depart.</a></td>';
-		s_str+='<td><a href="'+base_url+'places_nearby/'+'">Nearby</a></td>';
+		s_str+='<td><a href="'+getNewURI('/lines/', true, n.id)+'">Lignes</a></td>';
+		s_str+='<td><a href="'+getNewURI('/stop_areas/'+n.stop_area.id+'/', true, n.id)+'">StopArea</a></td>';
+		s_str+='<td><a href="'+getNewURI('/connections/', true, n.id)+'">Corresp.</a></td>';
+		s_str+='<td><a href="'+getNewURI('/departures/', true, n.id)+'">Depart.</a></td>';
+		s_str+='<td><a href="'+getNewURI('/places_nearby/', true, n.id)+'">Nearby</a></td>';
 		s_str+="</tr>\n";
 		str+=s_str;
-		//console.log(s_str);
 		coord=n.coord;
 		n.marker = L.marker([coord.lat, coord.lon]).addTo(map);
 		lamb=WGS_ED50(coord.lon, coord.lat);
@@ -229,8 +381,6 @@ function showPlacesNearbyHtml(){
 	str+='</tr>';
 	for (var i in ptref.object_list){
 		n=ptref.object_list[i];
-		base_url=location.href;
-		if (!base_url.endsWith(encodeURIComponent(n.id)+'/')) { base_url+=encodeURIComponent(n.id)+'/'; }
 		s_str="<tr>";
 		if (n.embedded_type=="poi") {
 			s_str+='<td>'+n.poi.poi_type.name + "</td>";
@@ -288,12 +438,10 @@ function showPOIsHtml(){
 	str+='</tr>';
 	for (var i in ptref.object_list){
 		n=ptref.object_list[i];
-		base_url=location.href;
-		if (!base_url.endsWith(encodeURIComponent(n.id)+'/')) { base_url+=encodeURIComponent(n.id)+'/'; }
 		s_str="<tr>";
-		s_str+='<td><a href="'+base_url+'">'+n.id+'</a></td>';
+		s_str+='<td><a href="'+getNewURI('', true, n.id)+'">'+n.id+'</a></td>';
 		s_str+='<td>'+n.label + "</td>";
-		s_str+='<td><a href="'+base_url+'places_nearby/'+'">Nearby</a></td>';
+		s_str+='<td><a href="'+getNewURI('/places_nearby/', true, n.id)+'">Nearby</a></td>';
 		s_str+="</tr>\n";
 		str+=s_str;
 		coord=n.coord;
@@ -323,12 +471,10 @@ function showPoiTypesHtml(){
 	str+='</tr>';
 	for (var i in ptref.object_list){
 		n=ptref.object_list[i];
-		base_url=location.href;
-		if (!base_url.endsWith(encodeURIComponent(n.id)+'/')) { base_url+=encodeURIComponent(n.id)+'/'; }
 		s_str="<tr>";
-		s_str+='<td><a href="'+base_url+'">'+n.id+'</a></td>';
+		s_str+='<td><a href="'+getNewURI('', true, n.id)+'">'+n.id+'</a></td>';
 		s_str+='<td>'+n.name + "</td>";
-		s_str+='<td><a href="'+base_url+'/pois/'+'">POIs</a></td>';
+		s_str+='<td><a href="'+getNewURI('/pois/', true, n.id)+'">POIs</a></td>';
 		s_str+="</tr>\n";
 		str+=s_str;
 	}
@@ -444,26 +590,41 @@ function showConnectionsHtml(){
 }
 
 function showLinesHtml(){
+    disruptions = [];
+    for (var di in ptref.disruption_list){
+        disruption = ptref.disruption_list[di];
+        disruptions[disruption.id] = disruption; 
+    }
 	str="";
 	str+='<table><tr>';
 	str+='<th>Id (Nb : ' + ptref.object_list.length + ' / ' + ptref.object_count + ')</th>';
 	str+='<th>Code</th>';
 	str+='<th>Name</th>';
 	str+='<th>Explorer</th>';
+	str+='<th></th>';
 	str+='</tr>';
 	newBounds=false;
 	for (var i in ptref.object_list){
 		n=ptref.object_list[i];
-		base_url=location.href;
-		if (!base_url.endsWith(encodeURIComponent(n.id+'/'))) { base_url+=encodeURIComponent(n.id+'/'); }
 		s_str="<tr>";
-		s_str+='<td><a href="'+base_url+'">'+n.id+'</a></td>';
+		s_str+='<td><a href="'+getNewURI('', true, n.id)+'">'+n.id+'</a></td>';
 		s_str+='<td>'+"<span class='icon-ligne' style='background-color: #"+n.color+";'>"+n.code + "</span>" +"</td>";
 		s_str+='<td>'+n.name + "</td>";
-		s_str+='<td><a href="'+base_url+encodeURIComponent('/physical_modes/')+'">Modes Ph</a></td>';
-		s_str+='<td><a href="'+base_url+encodeURIComponent('/commercial_modes/')+'">Modes Co</a></td>';
-		s_str+='<td><a href="'+base_url+encodeURIComponent('/stop_areas/')+'">Zones d\'arrêts</a></td>';
-		s_str+='<td><a href="'+base_url+encodeURIComponent('/routes/')+'">Routes</a></td>';
+		s_str+='<td><a href="'+getNewURI('/physical_modes/', true, n.id)+'">Modes Ph</a></td>';
+		s_str+='<td><a href="'+getNewURI('/commercial_modes/', true, n.id)+'">Modes Co</a></td>';
+		s_str+='<td><a href="'+getNewURI('/stop_areas/', true, n.id)+'">Zones d\'arrêts</a></td>';
+		s_str+='<td><a href="'+getNewURI('/routes/', true, n.id)+'">Routes</a></td>';
+        worst_disruption = "";
+        for (var k in n.links){
+            d= n.links[k];
+            //on cherche la severité la plus forte : NO_SERVICE
+            if (d.type == 'disruption') {
+                if ((worst_disruption == "") || (worst_disruption.severity.effect != 'NO_SERVICE')) {
+                    worst_disruption = disruptions[d.id];
+                }
+            }
+        }
+		s_str+='<td>'+getSeverityIcon(worst_disruption)+'</td>';
 		s_str+="</tr>\n";
 		str+=s_str;
 		//on trace la ligne sur la carte
@@ -492,15 +653,12 @@ function showRoutesHtml(){
 	newBounds=false;
 	for (var i in ptref.object_list){
 		n=ptref.object_list[i];
-		base_url=location.href;
-		if (!base_url.endsWith(encodeURIComponent(n.id+'/'))) { base_url+=encodeURIComponent(n.id+'/'); }
-
 		s_str="<tr>";
-		s_str+='<td><a href="'+base_url+'">'+n.id+'</a></td>';
+		s_str+='<td><a href="'+getNewURI('', true, n.id)+'">'+n.id+'</a></td>';
 		s_str+='<td>'+"<span class='icon-ligne' style='background-color: #"+n.color+";'>"+n.code + "</span>" +"</td>";
 		s_str+='<td>'+n.name + "</td>";
-		s_str+='<td><a href="'+base_url+encodeURIComponent('/stop_areas/')+'">Zones d\'arrêts</a></td>';
-		s_str+='<td><a href="'+base_url+encodeURIComponent('/vehicle_journeys/')+'">Courses</a></td>';
+		s_str+='<td><a href="'+getNewURI('/stop_areas/', true, n.id)+'">Zones d\'arrêts</a></td>';
+		s_str+='<td><a href="'+getNewURI('/vehicle_journeys/', true, n.id)+'">Courses</a></td>';
 		s_str+="</tr>\n";
 		str+=s_str;
 		//on trace le parcours sur la carte
@@ -538,6 +696,8 @@ function showObjectHtml(ptref){
 		showDeparturesHtml();
 	} else if (ptref.object_type == "places_nearby") {
 		showPlacesNearbyHtml();
+	} else if (ptref.object_type == "traffic_reports") {
+		showTrafficReportsHtml();
 	} else if (ptref.object_type == "networks") {
 		showNetworksHtml();
 	} else {
@@ -551,7 +711,7 @@ function showAriane(uri){
 	ariane="";
 	object_type = "";
 	key_list=["networks", "lines", "routes", "commercial_modes", "physical_modes", "stop_areas", "stop_points",
-	  "connections", "departures", "places_nearby"];
+	  "connections", "departures", "places_nearby", "traffic_reports"];
 	for (var i=0; i<res.length; i++) {
 		if (res[i]!="") {
 			if (key_list.indexOf(res[i])>=0) {
